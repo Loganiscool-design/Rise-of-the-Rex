116531c08240bbddedb312f8e99b0ff3102297e8
Loganiscool-design <logandaugherty444@gmail.com>
2026-02-20 10:51:32 -0700

    finished dino script
    26:14 in video

--- rise-of-the-rex/scenes/main.gd
+++ rise-of-the-rex/scenes/main.gd
@@ -18,19 +18,22 @@ func _ready():
 
 func new_game():
 	
-	score = 0 
-	#reset the nodes
+	score = 0
+	show_score()
 	$Dino.position = DINO_START_POS
 	$Dino.velocity = Vector2i (0, 0)
 	$Camera2D.position = CAM_START_POS
 	$Ground.position = Vector2i(0, 0)
 
+
+
+	$HUD.get_node("StartLabel").show()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if game_running:
 		speed = START_SPEED
-			
-			
+		
+		
 		$Dino.position.x += speed
 		$Camera2D.position.x += speed
 		
@@ -38,21 +41,15 @@ func _process(delta):
 		
 		score += speed
 		show_score()
-
+			
+			
 		if $Camera2D.position.x - $Ground.position.x > screen_size.x * 1.5:
 			$Ground.position.x += screen_size.x
-	
-		
+	else:
+		if Input.is_action_pressed("ui_accept"):
+			game_running = true
+			$HUD.get_node("StartLabel").hide()
 		
 func show_score():
-	$HUD.get_node("ScoreLabel").text = "score: " + str(score / SCORE_MODIFIER)
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	$HUD.get_node("ScoreLabel").text = "SCORE: " + str(score / SCORE_MODIFIER)
+		
