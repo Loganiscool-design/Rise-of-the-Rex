f3ba9c5dea892437e65757adfd69b06a8bf4a317
Loganiscool-design <logandaugherty444@gmail.com>
2026-02-23 16:09:17 -0700

    almost done
    45:05

--- rise-of-the-rex/scenes/main.gd
+++ rise-of-the-rex/scenes/main.gd
@@ -16,6 +16,8 @@ var bird_heights := [200, 390]
 
 const DINO_START_POS := Vector2i(150, 485)
 const CAM_START_POS := Vector2i(576, 324)
+var difficulty
+const MAX_DIFFICULTY : int = 2
 var score : int
 const SCORE_MODIFIER : int = 10
 var speed : float
@@ -37,6 +39,10 @@ func new_game():
 	
 	score = 0
 	show_score()
+	game_running = false
+	difficulty = 0
+	
+	
 	$Dino.position = DINO_START_POS
 	$Dino.velocity = Vector2i (0, 0)
 	$Camera2D.position = CAM_START_POS
@@ -48,9 +54,12 @@ func new_game():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if game_running:
+			
 		speed = START_SPEED + score / SPEED_MODIFIER
 		if speed > MAX_SPEED:
 			speed = MAX_SPEED
+		adjust_difficulty()
+			
 		
 		generate_obs()
 		
@@ -66,22 +75,64 @@ func _process(delta):
 			
 		if $Camera2D.position.x - $Ground.position.x > screen_size.x * 1.5:
 			$Ground.position.x += screen_size.x
+			
+		for obs in obstacles: 
+			if obs.position.x < ($Camera2D.position.x - screen_size.x):
+				remove_obs(obs)
+			
+			
+			
 	else:
 		if Input.is_action_pressed("ui_accept"):
 			game_running = true
 			$HUD.get_node("StartLabel").hide()
 		
 func generate_obs():
-	if obstacles.is_empty():
+	if obstacles.is_empty() or last_obs.position.x < score + randi_range(300, 500):
 		var obs_type = obstacle_types[randi() % obstacle_types.size()]
 		var obs
-		obs = obs_type.instantiate()
-		var obs_height = obs.get_node("Sprite2D").texture.get_height()
-		var obs_scale = obs.get_node("Sprite2D").scale
-		last_obs = obs
-		add_child(obs)
-		obstacles.append(obs)
+		var max_obs = difficulty + 1
+		for i in range(randi() % max_obs + 1): 
+			obs = obs_type.instantiate()
+			var obs_height = obs.get_node("Sprite2D").texture.get_height()
+			var obs_scale = obs.get_node("Sprite2D").scale
+			var obs_x : int = screen_size.x + score + 100 + (i * 100)
+			var obs_y : int = screen_size.y - ground_height - (obs_height * obs_scale.y / 2) + 5
+			last_obs = obs
+			add_obs(obs, obs_x, obs_y)
+		
+		if difficulty == MAX_DIFFICULTY: 
+			if (randi() % 2) == 0:
+				
+				obs = bird_scene.instantiate()
+				var obs_x : int = screen_size.x + score + 100 
+				var obs_y : int = bird_heights[randi() % bird_heights.size()]
+				add_obs(obs, obs_x, obs_y)
+		
+		
+func add_obs(obs, x, y):
+	obs.position = Vector2i(x, y)
+	obs.body_entered.connect(hit_obs)
+	add_child(obs)
+	obstacles.append(obs)
+	
+func remove_obs(obs):
+	obs.queue_free()
+	obstacles.erase(obs)
+	
+func hit_obs(body):
+	if body.name == "Dino":
+		game_over()
+	
 		
 func show_score():
 	$HUD.get_node("ScoreLabel").text = "SCORE: " + str(score / SCORE_MODIFIER)
 		
+func adjust_difficulty():
+	difficulty = score / SPEED_MODIFIER
+	if difficulty > MAX_DIFFICULTY: 
+		difficulty = MAX_DIFFICULTY
+	
+func game_over():
+	get_tree().paused = true
+	game_running = false
