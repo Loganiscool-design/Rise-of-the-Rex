4f8c2c7d71be1de0ad07db502b66a27b1901bd65
Loganiscool-design <logandaugherty444@gmail.com>
2026-02-23 10:52:06 -0700

    Almost done
    31:44

--- rise-of-the-rex/scenes/main.gd
+++ rise-of-the-rex/scenes/main.gd
@@ -1,6 +1,19 @@
 extends Node
 
 
+var stump_scene = preload("res://scenes/stump.tscn")
+var rock_scene = preload("res://scenes/rock.tscn")
+var barrel_scene = preload("res://scenes/barrel.tscn")
+var bird_scene = preload("res://scenes/bird.tscn")
+var obstacle_types := [stump_scene, rock_scene, barrel_scene]
+var obstacles : Array
+var bird_heights := [200, 390]
+
+
+
+
+
+
 const DINO_START_POS := Vector2i(150, 485)
 const CAM_START_POS := Vector2i(576, 324)
 var score : int
@@ -8,12 +21,16 @@ const SCORE_MODIFIER : int = 10
 var speed : float
 const START_SPEED : float = 10.0
 const MAX_SPEED : int = 25
+const SPEED_MODIFIER : int  = 5000
 var screen_size : Vector2i
+var ground_height : int 
 var game_running : bool
+var last_obs
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_window().size
+	ground_height = $Ground.get_node("Sprite2D").texture.get_height()
 	new_game()
 
 func new_game():
@@ -31,7 +48,11 @@ func new_game():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if game_running:
-		speed = START_SPEED
+		speed = START_SPEED + score / SPEED_MODIFIER
+		if speed > MAX_SPEED:
+			speed = MAX_SPEED
+		
+		generate_obs()
 		
 		
 		$Dino.position.x += speed
@@ -49,6 +70,17 @@ func _process(delta):
 		if Input.is_action_pressed("ui_accept"):
 			game_running = true
 			$HUD.get_node("StartLabel").hide()
+		
+func generate_obs():
+	if obstacles.is_empty():
+		var obs_type = obstacle_types[randi() % obstacle_types.size()]
+		var obs
+		obs = obs_type.instantiate()
+		var obs_height = obs.get_node("Sprite2D").texture.get_height()
+		var obs_scale = obs.get_node("Sprite2D").scale
+		last_obs = obs
+		add_child(obs)
+		obstacles.append(obs)
 		
 func show_score():
 	$HUD.get_node("ScoreLabel").text = "SCORE: " + str(score / SCORE_MODIFIER)
